100e4fae1311f2f82a3fee1e2bfe338a2f295f9e
Alex Darigan <47497501+AlexDarigan@users.noreply.github.com>
2025-06-02 23:24:51 +0100

    Delete requests/add directory

--- requests/add/application/main.tf
+++
@@ -1,9 +0,0 @@
-data "http" "add_prowlarr_application" {
-  url    = "http://192.168.1.12:${var.prowlarr.node_port}/${var.request.route}"
-  method = var.request.method
-  request_headers = {
-    "Content-Type": var.request.content_type,
-    "X-Api-Key": var.prowlarr.api_key
-  }
-  request_body = local.body
-}        
--- requests/add/download_client/main.tf
+++
@@ -1,9 +0,0 @@
-data "http" "add_download_client" {
-  url    = "http://192.168.1.12:${var.application.node_port}/${var.request.route}"
-  method = var.request.method
-  request_headers = {
-    "Content-Type": var.request.content_type,
-    "X-Api-Key": var.application.api_key
-  }
-  request_body = local.body
-}
